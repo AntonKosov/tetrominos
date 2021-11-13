101128ea40707a9850700da0eb8d4a388668a12e
Anton Kosov <AntonRKosov@gmail.com>
2021-11-13 16:12:19 -0600

    #26 Build for linux.arm64

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ golangci-lint run || { echo 'lint failed' ; exit 1; }
 rm ./build/*
 env GOOS=windows GOARCH=amd64 go build -o ./build/tetrominos.windows.amd64.exe .
 env GOOS=linux GOARCH=amd64 go build -o ./build/tetrominos.linux.amd64 .
+env GOOS=linux GOARCH=arm64 go build -o ./build/tetrominos.linux.arm64 .
 env GOOS=darwin GOARCH=amd64 go build -o ./build/tetrominos.darwin.amd64 .
